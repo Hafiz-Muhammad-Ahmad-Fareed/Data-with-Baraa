5eca6dc9ed92be183a506c07482d89fd4c342253
Hafiz-Muhammad-Ahmad-Fareed <hafizmuhammadahmadfarid@gmail.com>
2026-02-25 19:40:02 +0500

    Add Python introduction content and examples

---
+++ Python Full Course for Beginners–From Zero to Hero/1-Introduction/main.py
@@ -0,0 +1,30 @@
+# single-line comment
+"""
+This is a multi-line comment
+"""
+
+# escape sequences
+"""
+\", \', \\, \n, \t, \b 
+"""
+
+# Output
+print("Hello World!")
+print('Hello World!')
+print("Hello \'World\'!")
+print('Hello \"World\"!')
+print("Hello \"World\"!")
+print('Hello \'World\'!')
+print("Path: C:\\users\\desktop")
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("Hello\b World")
+print("Your Learning Path:\n - Python Basic\n - Data Engineering\n - AI")
+print("""Your Learning Path: 
+ - Python Basic
+ - Data Engineering 
+ - AI""")
+
+# Input
+name = input("Enter your name!")
+print(f"your name is {name}")
